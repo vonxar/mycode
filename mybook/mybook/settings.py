39cf8194bbd54245181faacc8eaaba1b5e50e818
vonxar <p02d1108@gmail.com>
2020-08-22 22:21:41 +0900

    python django 勉強 キー管理

--- mybook/mybook/settings.py
+++ mybook/mybook/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .settings_local import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '84g=igt&q)@gj*9j&h0+htwp_^&$i^gx_nas8jt$6(m@d3^m54'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .settings_local import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2!=zlaj=v+35f!#mp7ifaa^i1__dy30x9y&t=ew@&jw909ez@o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
